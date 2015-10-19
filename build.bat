b9350e03fb4c6fdb9835c12011f13c3849024e7d
Mindaugas Pielikis <m.pielikis@gmail.com>
2015-10-19 08:59:47 +0300

    Update build.bat

--- build.bat
+++ build.bat
@@ -2,4 +2,4 @@
 
 cls
 .nuget\NuGet.exe restore
-"%ProgramFiles(x86)%\MSBuild\12.0\Bin\MSBuild.exe" /t:Rebuild
+"%ProgramFiles(x86)%\MSBuild\12.0\Bin\MSBuild.exe" /t:Rebuild /p:Configuration=Debug /p:Platform="Any CPU"
